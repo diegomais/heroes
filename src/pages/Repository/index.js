c6a25d7d641fdf91a9b8be69be3546871f869bb5
Diego Mais <diegomais@live.com>
2019-07-14 08:19:25 -0300

    Add load data from API
    - This improvement provides app to load repository details from GitHub API.
    
    On branch master
    Changes to be committed:
    	modified:   package.json
    	modified:   src/pages/Repository/index.js

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: { state: 'open', per_page: 5 },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    return <h1>Repository</h1>;
+  }
 }
